92dc4d811ac465005f2144f834122bfa3f1c49dd
Shodmonov2006 <olimkhujashodmonovv@gmail.com>
2023-03-16 10:14:21 +0300

    the end

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const myName = 'Jonas'
+
+function first() {
+
+    if (age >= 30) {
+        const decade = 3;
+        var millenimal = true
+    }
+
+
+    function second() {
+        const job = 'teacher'
+
+        console. log('${myName} is a ${age}-ald ${job}')
+
+    }
+    second()
+}
